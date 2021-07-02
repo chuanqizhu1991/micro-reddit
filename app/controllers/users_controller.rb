e91c4a9df22bc9c4fb70c70bebab77ec87994573
chuanqizhu <chuanqizhu1991@outlook.com>
2021-07-02 17:31:33 +0800

    add show action, be able to show each user's page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def show
+    # params is the hash captured from the request path"
+    @user = User.find(params[:id]) 
+  end
 end
